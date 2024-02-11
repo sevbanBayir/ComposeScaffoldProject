8925acda77250778cbdcb05df4b3c48a5bfac126
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-11 16:30:45 +0300

    made adjustments to return types of RetrofitService.kt

--- core/network/src/main/java/com/sevban/network/source/remote/RemoteDataSourceImpl.kt
+++ core/network/src/main/java/com/sevban/network/source/remote/RemoteDataSourceImpl.kt
@@ -3,16 +3,18 @@ package com.sevban.network.source.remote
 import com.sevban.network.source.model.CharacterDTO
 import com.sevban.network.source.model.CharactersDTO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class RemoteDataSourceImpl @Inject constructor(private val apiService: RetrofitService) :
     RemoteDataSource {
 
-    override suspend fun getAllCharacters(): Flow<CharactersDTO> {
-        return apiService.getAllCharacters()
+    override suspend fun getAllCharacters(): Flow<CharactersDTO> = flow {
+        emit(apiService.getAllCharacters())
     }
 
-    override suspend fun getCharacterById(id: String): Flow<CharacterDTO> {
-        return apiService.getSingleCharacter(id)
+
+    override suspend fun getCharacterById(id: String): Flow<CharacterDTO> = flow {
+        emit(apiService.getCharacterById(id))
     }
 }
