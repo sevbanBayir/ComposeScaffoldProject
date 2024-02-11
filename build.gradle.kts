fd02ddce14dfde0f7afe0cdee8a31eeca96cd89f
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-11 15:45:40 +0300

    added retrofit and hilt to network module

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.androidLibrary) apply false
+
+    alias(libs.plugins.dagger.hilt) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
 }
