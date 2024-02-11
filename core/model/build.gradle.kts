3e02b0416298126cd4650b0fd90dc9642f132c2f
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-11 17:01:47 +0300

    updated java_version

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,10 +1,10 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id("java-library")
+    id(libs.plugins.java.library.get().pluginId)
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
