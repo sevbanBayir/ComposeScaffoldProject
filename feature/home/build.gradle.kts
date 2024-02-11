38572eb67705e612c1ae0bd45ab4879314f0d323
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-11 22:35:53 +0300

    -hilt & compose dependencies added

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -34,6 +36,32 @@ android {
 }
 
 dependencies {
+
+    // --------------Compose--------------
+    // Compose-Ui
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+
+    // Compose-Testing
+    androidTestImplementation(libs.compose.ui.testing.junit4)
+    androidTestImplementation(platform(libs.compose.bom))
+
+    //Compose-Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    //Compose-Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //------------------------------------
+
+    // Dagger-Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    kapt(libs.hilt.android.compiler)
 
     implementation(libs.androidx.ktx)
     implementation(libs.appcompat)
