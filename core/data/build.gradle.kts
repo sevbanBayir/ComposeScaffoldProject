cbd3f790c38a9c11e39d57281e86ca391057e8f5
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-17 13:06:25 +0300

    removed material dependency

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
 
     implementation(libs.androidx.ktx)
     implementation(libs.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
