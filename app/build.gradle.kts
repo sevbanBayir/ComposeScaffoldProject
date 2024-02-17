dbe999f1cdb70340cd7da411cb0a6f598a399949
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-17 21:08:12 +0300

    created core-ui module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(projects.core.data)
     implementation(projects.core.model)
     implementation(projects.core.domain)
+    implementation(projects.core.ui)
     implementation(projects.feature.home)
     implementation(projects.feature.detail)
 
