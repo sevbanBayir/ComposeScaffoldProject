887997bce23bf95b7575c33185334927fed5604c
Sevban Bayır <sevbanbuyer@gmail.com>
2024-02-18 18:57:51 +0300

    removed suspend keyword from data source & RepositoryImpl.kt

--- core/data/src/main/java/com/sevban/data/repository/RepositoryImpl.kt
+++ core/data/src/main/java/com/sevban/data/repository/RepositoryImpl.kt
@@ -4,26 +4,25 @@ import com.sevban.data.mapper.toCharacter
 import com.sevban.model.Character
 import com.sevban.network.source.remote.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteDataSource
 ) : Repository {
-    override fun getCharacterList(): Flow<List<Character>> = flow {
+    override fun getCharacterList(): Flow<List<Character>> =
         remoteDataSource.getAllCharacters().map {
             it.results!!.map { characterDTO ->
                 characterDTO.toCharacter()
             }
         }
-    }
 
-    override fun getCharacterById(id: String): Flow<Character> = flow {
+
+    override fun getCharacterById(id: String): Flow<Character> =
         remoteDataSource.getCharacterById(id).map { characterDTO ->
-                characterDTO.toCharacter()
+            characterDTO.toCharacter()
         }
-    }
+
 }
 
 // TODO: Naming for repository, dataSources etc. should be reconsidered.
